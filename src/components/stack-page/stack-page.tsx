7b5a3744a0d766b8e63f6f268854e3c92ad1c199
Annebula77 <goharikv@gmail.com>
2023-07-28 02:47:27 +0300

    converted to classes stack queue

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import styles from './stack.module.css';
 import { Input } from "../ui/input/input";
@@ -7,96 +7,103 @@ import { Circle } from "../ui/circle/circle";
 import { ElementStates, StackArr } from "../../types/element-states";
 import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
+interface State {
+  isLoading: boolean;
+  symbol: string;
+  stackArray: StackArr[];
+}
 
-export const StackPage: React.FC = () => {
-
-  const [isLoading, setIsLoading] = useState(false);
-  const [symbol, setSymbol] = useState<number | string | null>('');
-  const [stackArray, setStackArray] = useState<StackArr[]>([]);
-
-  const handleAdd = useCallback(() => {
-    if (symbol !== '' && symbol !== null) {
-      setIsLoading(true);
+class StackPage extends React.Component<{}, State> {
+  state: State = {
+    isLoading: false,
+    symbol: '',
+    stackArray: []
+  };
+  handleAdd = () => {
+    if (this.state.symbol !== '') {
+      this.setState({ isLoading: true });
       setTimeout(() => {
-        setStackArray(prevState => [...prevState, { value: symbol, type: ElementStates.Changing }]);
-        setSymbol(null);
-        setIsLoading(false);
+        this.setState(prevState => ({
+          stackArray: [...prevState.stackArray, { value: prevState.symbol, type: ElementStates.Changing }],
+          symbol: '',
+          isLoading: false
+        }));
 
         setTimeout(() => {
-          setStackArray(prevState => {
-            let newState = [...prevState];
+          this.setState(prevState => {
+            let newState = [...prevState.stackArray];
             newState[newState.length - 1].type = ElementStates.Default;
-            return newState;
+            return { stackArray: newState };
           });
         }, SHORT_DELAY_IN_MS);
       }, SHORT_DELAY_IN_MS);
     }
-  }, [symbol]);
-
-  const handleDelete = useCallback(() => {
-    if (stackArray.length > 0) {
-      setIsLoading(true);
-      setStackArray(prevState => {
-        let newState = [...prevState];
-        newState[newState.length - 1].type = ElementStates.Changing;
-        return newState;
-      });
-      setTimeout(() => {
-        setStackArray(prevState => {
-          let newState = [...prevState];
-          newState.pop();
-          return newState;
-        });
-        setIsLoading(false);
-      }, SHORT_DELAY_IN_MS);
-    }
-  }, [stackArray.length]);
-
-  const handleClear = useCallback(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setStackArray([]);
-      setIsLoading(false);
-    }, SHORT_DELAY_IN_MS);
-  }, []);
-
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSymbol(event.target.value);
   }
 
+  handleDelete = () => {
+    if (this.state.stackArray.length > 0) {
+      this.setState(prevState => {
+        let newState = [...prevState.stackArray];
+        newState[newState.length - 1].type = ElementStates.Changing;
+        return { stackArray: newState, isLoading: true };
+      });
 
-  return (
-    <SolutionLayout title="Стек">
-      <div className={styles.content}>
-        <div className={styles.input}>
-          <Input
-            extraClass={styles.input__extra}
-            maxLength={4}
-            onChange={handleInputChange}
-            value={symbol || ''}
-            disabled={isLoading}
-          />
-          <p className={styles.text}>Максимум — 4 символа</p>
-        </div>
-        <div className={styles.boxes}>
-          <Button text="Добавить" onClick={handleAdd} disabled={isLoading} />
-          <Button text="Удалить" onClick={handleDelete} disabled={isLoading} />
-        </div>
-        <Button text="Очистить" onClick={handleClear} disabled={isLoading} />
-      </div>
-      <div className={styles.circle__container}>
-        {stackArray.map((item, index) =>
-          <Circle
-            key={index}
-            letter={item.value?.toString() || ''}
-            index={index}
-            head={index === stackArray.length - 1 ? 'top' : null}
-            state={item.type}
-          />
-        )}
-      </div>
+      setTimeout(() => {
+        this.setState(prevState => {
+          let newState = [...prevState.stackArray];
+          newState.pop();
+          return { stackArray: newState, isLoading: false };
+        });
+      }, SHORT_DELAY_IN_MS);
+    }
+  }
 
-    </SolutionLayout>
-  );
+  handleClear = () => {
+    this.setState({ isLoading: true });
+    setTimeout(() => {
+      this.setState({ stackArray: [], isLoading: false });
+    }, SHORT_DELAY_IN_MS);
+  }
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ symbol: event.target.value });
+  }
+
+  render() {
+    return (
+      <SolutionLayout title="Стек">
+        <div className={styles.content}>
+          <div className={styles.input}>
+            <Input
+              extraClass={styles.input__extra}
+              maxLength={4}
+              onChange={this.handleInputChange}
+              value={this.state.symbol}
+              disabled={this.state.isLoading}
+            />
+            <p className={styles.text}>Максимум — 4 символа</p>
+          </div>
+          <div className={styles.boxes}>
+            <Button text="Добавить" onClick={this.handleAdd} disabled={this.state.isLoading} />
+            <Button text="Удалить" onClick={this.handleDelete} disabled={this.state.isLoading} />
+          </div>
+          <Button text="Очистить" onClick={this.handleClear} disabled={this.state.isLoading} />
+        </div>
+        <div className={styles.circle__container}>
+          {this.state.stackArray.map((item, index) =>
+            <Circle
+              key={index}
+              letter={item.value?.toString() || ''}
+              index={index}
+              head={index === this.state.stackArray.length - 1 ? 'top' : null}
+              state={item.type}
+            />
+          )}
+        </div>
+
+      </SolutionLayout>
+    );
+  }
 };
+
+export { StackPage };
